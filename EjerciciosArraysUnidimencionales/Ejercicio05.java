7da36c0a875a1edf453f934ab0904937bff5b5b3
Franck <202012672i@utea.edu.pe>
2024-11-29 20:03:03 -0500

    commit

--- EjerciciosArraysUnidimencionales/Ejercicio05.java
+++ EjerciciosArraysUnidimencionales/Ejercicio05.java
@@ -7,6 +7,54 @@ public class Ejercicio05 {
     quiere destacar el máximo o el mínimo. Seguidamente se volverá a mostrar el array
     escribiendo el número destacado entre dobles asteriscos.*/
     public static void main(String[] args) {
-        
+        int[] numeros = new int[100];
+        //Ingresar numeros del (0 al 100) de forma automatica
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = (int) (Math.random() * 501);
+            }
+        //Imprimir el arreglo generado
+        for (int i = 0; i < numeros.length; i++) {
+        System.out.print( numeros[i] + " ");
+        }
+        //Pedir si quiere resaltar el maximo o el minimo
+        System.out.println(" ");
+        System.out.println("Que valor quieres Resaltar: 1 = minimo, 2 = maximo");
+        int aux = Integer.parseInt(System.console().readLine());
+        //Definir cual es el numero de maximo o minimo valor
+        int min = 500 ;
+        int max = 0;
+        if (aux == 1) {
+            for (int i = 0; i < numeros.length; i++) {
+                if (min > numeros[i] ) {
+                    min = numeros[i];
+                }
+            }
+        }
+        if (aux ==  2) {
+            for (int i = 0; i < numeros.length; i++) {
+                if (max < numeros[i]) {
+                    max = numeros[i];
+                }
+            }
+        } 
+        //Imprimir el arreglo resaltando el maximo o minimo
+        if (aux == 1) {
+            for (int i = 0; i < numeros.length; i++) {
+                if (numeros[i] == min) {
+                    System.out.print(" ** " + numeros[i] + " ** ");
+                } else{
+                    System.out.print(" " + numeros[i] + " ");
+                }
+            }
+        }
+        if (aux == 2) {
+            for (int i = 0; i < numeros.length; i++) {
+                if (numeros[i] == max) {
+                    System.out.print(" ** " + numeros[i] + " ** ");
+                } else{
+                    System.out.print(" " + numeros[i] + " ");
+                }
+            }
+        }
     }
 }

28201edcec0fe2315b8e50091ef31570921fca72
Fr2nck <202012672i@utea.edu.pe>
2024-12-02 10:00:15 -0500

    commit

---
+++ Arrays/ArrayBi/ArrayBi.java
@@ -0,0 +1,8 @@
+package Arrays.ArrayBi;
+public class ArrayBi {
+                    /*Un array bibimencional es una estructura que usa dos
+                    indices para localizar un elemento */
+    public static void main(String[] args) {
+        
+    }
+}

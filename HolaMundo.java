364e621cdf152955a68f008845624a4bdbf43c16
Fr2nck <202012672i@utea.edu.pe>
2024-12-04 10:58:39 -0500

    commit

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,9 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!!");
-    }
+        int a = Integer.MAX_VALUE;
+        System.out.println("El valor maximo es: " + a);
+        int b = Integer.MIN_VALUE;
+        System.out.println("El valor minimo es: " + b);
+    }    
 } 

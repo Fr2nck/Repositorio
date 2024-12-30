908e3b63769d5f18790f27e2e26d68bcb1804d7f
Fr2nck <202012672i@utea.edu.pe>
2024-12-30 11:54:11 -0500

    commit

--- Poo/Cubo/PruevaCubo.java
+++ Poo/Cubo/PruevaCubo.java
@@ -6,8 +6,8 @@ public class PruevaCubo {
         Cubo cubo03 = new Cubo(15, 10);
 
         //Indicar el numero de cubo, su capacidad  y el contenido que ya contiene
-        System.out.println("El primer cubo tiene la capacidad de almacenar: " + cubo01.capacidad + " Litros y contiene " + cubo01.contenido);
-        System.out.println("El primer cubo tiene la capacidad de almacenar: " + cubo02.capacidad + " Litros y contiene " + cubo02.contenido);
-        System.out.println("El primer cubo tiene la capacidad de almacenar: " + cubo03.capacidad + " Litros y contiene " + cubo03.contenido);
+        System.out.println("El primer cubo tiene una capacidad de : " + cubo01.capacidad + " Litros y contiene " + cubo01.contenido);
+        System.out.println("El sgundo cubo tiene una capacidad de : " + cubo02.capacidad + " Litros y contiene " + cubo02.contenido);
+        System.out.println("El tercer cubo tiene una capacidad de : " + cubo03.capacidad + " Litros y contiene " + cubo03.contenido);
     }
 }

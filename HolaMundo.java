f575170e5c4f04bfb3df7c3814bb4ddd9d450641
Franck <202012672i@utea.edu.pe>
2025-01-24 20:06:34 -0500

    commit

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,9 +1,5 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!!");
-        int a = Integer.MAX_VALUE;
-        System.out.println("El valor maximo es: " + a);
-        int b = Integer.MIN_VALUE;
-        System.out.println("El valor minimo es: " + b);
     }        
 } 

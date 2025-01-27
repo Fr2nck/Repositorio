b24e0bf06010d1f912ed16d5cc71251342439120
Fr2nck <202012672i@utea.edu.pe>
2025-01-27 10:10:21 -0500

    commit

---
+++ Ficheros/Fichero01.java
@@ -0,0 +1,15 @@
+package Ficheros;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Fichero01 {
+    public static void main(String[] args) {
+        
+
+        
+
+    }
+}
